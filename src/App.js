c9ca21c2d8c566c792dd7213866c14d197f65015
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2021-03-03 16:17:43 -0500

    home page test layout, fonts, colors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './assets/fonts/fonts.css';
+import Home from './components/Home';
+// import Navbar from './components/Navbar';
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('white');
+
+  function changeBackground(value) {
+    setBackgroundColor(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`w-screen h-screen ${backgroundColor}`}>
+      <Home changeBackground={changeBackground} />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// imports
+
+function Navbar() {
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Navbar;
